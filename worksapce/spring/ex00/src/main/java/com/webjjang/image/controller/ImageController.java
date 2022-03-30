389678f0906d5c2b4d5cc6b7499ccfe1b6f587ca
alenjb <alenjb@naver.com>
2022-03-30 22:23:59 +0900

    이미지 리스트 완성하기

--- worksapce/spring/ex00/src/main/java/com/webjjang/image/controller/ImageController.java
+++ worksapce/spring/ex00/src/main/java/com/webjjang/image/controller/ImageController.java
@@ -1,5 +1,6 @@
 package com.webjjang.image.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,15 +8,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.webjjang.image.mapper.ImageMapper;
+import com.webjjang.image.service.ImageService;
 import com.webjjang.image.vo.ImageVO;
 import com.webjjang.util.PageObject;
 
+import lombok.extern.log4j.Log4j;
+
 @Controller
 @RequestMapping("/image")
+@Log4j
 public class ImageController {
+	//자동 DI
+	@Autowired
+	private ImageService service;
 	
 	@GetMapping("/list.do")
 	public String list(PageObject pageObject, Model model) throws Exception{
+		log.info("list()");
+		
+		model.addAttribute("list", service.list(pageObject));
 		return "image/list";
 	}
 	@PostMapping("/changeImage.do")
