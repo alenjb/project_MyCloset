74f2ce49a6acbc9743032fee0bf4277fa617d639
alenjb <alenjb@naver.com>
2022-09-13 22:19:34 +0900

    <피팅룸 구현-5>
    - FittingMapper.xml에서 selectKey가 null로 반환되는 오류 발생 <- 이 오류를 해결하지 못하면 더 이상 진행 불가
    - FittingController에서 기본적인 기능 ClosetController에서 가져와 비슷하게 바꾸는 작업 완료

--- worksapce/spring/MyCloset/src/main/java/com/mycloset/fitting/controller/FittingController.java
+++ worksapce/spring/MyCloset/src/main/java/com/mycloset/fitting/controller/FittingController.java
@@ -11,12 +11,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import com.mycloset.closet.vo.ClosetVO;
 import com.mycloset.fitting.service.FittingService;
 import com.mycloset.fitting.vo.FittingVO;
 import com.mycloset.fitting.vo.ImageVO;
 import com.mycloset.member.vo.LoginVO;
+import com.mycloset.util.Critera;
+import com.mycloset.util.PageDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -55,15 +57,49 @@ public class FittingController {
 		String memberId = loginVO.getMember_id();
 		// 모델에 아이디 담기
 		model.addAttribute("member_id", memberId);
+		System.out.println("vo: "+vo);
 		service.enroll(vo);
 		
 		return "redirect:list";
 	}
 
 	// 2. 피팅 리스트
-
+	@GetMapping("/list")
+	public void list(HttpServletRequest request, Critera cri, Model model) throws Exception {
+		// 로그인 정보를 받기
+		HttpSession session = request.getSession();
+		LoginVO loginVO = (LoginVO) session.getAttribute("login");
+		// 아이디 추출
+		String memberId = loginVO.getMember_id();
+		// 옷 리스트 가져오는 작업을 통해 closets에 리스트 형태로 저장(일단 임시로 0을 보냄)
+		// 0 자리는 아무 값이나 보내면 되서 0을 보냄
+		List<FittingVO> fittings = service.getListWithPaging(cri, 0);
+//		closets.forEach(b -> log.info(b));
+//		log.info(closets);
+		// 모델에 피팅 리스트 담기
+		model.addAttribute("fittings", fittings);
+		// 옷 피팅 개수 세기
+		int totalNum = service.getTotalNum();
+		// 페이지 관련 정보 담기
+		model.addAttribute("pageMaker", new PageDTO(cri, totalNum));
+	}
+	
 	// 3. 피팅 세부 보기
-
+	@GetMapping("/view")
+	public String view(HttpServletRequest request, Model model, @RequestParam("fitting_id")String fitting_id) throws Exception{
+		//로그인 정보를 받기
+		HttpSession session = request.getSession();
+		LoginVO loginVO = (LoginVO)session.getAttribute("login");
+		//아이디 추출
+		String memberId= loginVO.getMember_id();
+		//모델에 피팅 아이디 추가
+		model.addAttribute("fitting_id", fitting_id);
+		int cId = Integer.parseInt(fitting_id);
+		//서비스에서 view 메서드 호출
+		FittingVO vo= service.view(memberId, cId);
+		model.addAttribute("vo", vo);
+		return "fitting/view";
+	}
 	// 4. 피팅 수정
 	// 4-1. 피팅 수정 폼
 	// 4-2. 피팅 수정
